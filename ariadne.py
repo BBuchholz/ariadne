778500b74e6d1f4d3820c6002a1dbdec3cc406e8
Brent Buchholz <brentbuchholz@gmail.com>
2026-03-24 18:33:42 -0500

    Moved completed tests into a separate file

--- ariadne.py
+++ ariadne.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import re
 
 # Component -> Task
 class Task(ABC):
@@ -11,12 +12,22 @@ class Task(ABC):
     
     @description.setter
     def description(self, value):
+        # if not isinstance(value, str):
+        #     raise ValueError(f"description must be a string! RECEIVED: {value}")
+        # value = value.strip()
+        # if not value:
+        #     raise ValueError(f"description must not be empty or whitespace! RECEIVED: {value}")
+        # if re.search(r'[^a-zA-Z0-9\s]', value):
+        #     raise ValueError("String contains characters other than alphanumeric and spaces.")
+        self.validate_description(value)
+        self._description = value
+
+    def validate_description(self, value):
         if not isinstance(value, str):
             raise ValueError(f"description must be a string! RECEIVED: {value}")
         value = value.strip()
         if not value:
             raise ValueError(f"description must not be empty or whitespace! RECEIVED: {value}")
-        self._description = value
 
     @abstractmethod
     def show_details(self):
@@ -37,6 +48,11 @@ class ComplexTask(Task):
     def add_task(self, task): 
         self.child_tasks.append(task)
 
+    def validate_description(self, value):
+        super().validate_description(value)
+        if re.search(r'[^a-zA-Z0-9\s]', value):
+            raise ValueError("String contains characters other than alphanumeric and spaces.")
+
     def show_details(self):
         print(f"- [ ] {self.description}")
         for child_task in self.child_tasks:
@@ -54,58 +70,3 @@ if __name__ == '__main__':
     print("")
     print("TODO: make current indent change when tasks get added to each other and update show_details method to reflect current indent for a given task")
     print("")
-    print("these next steps should all throw an error and get caught properly, if it does not, its not working yet, we'll move them to an error catching block when its confirmed that they throw errors (this is a quick and dirty Red Green Test from TDD principles)")
-    print("")
-    print("If this script ends with 'An unknown error occurred' it means they weren't caught properly")
-    
-    # NONSTRING TASK VIOLATION
-    print("")
-    try:
-        print("trying...")
-        some_list = ["string", "list", "not supported"]
-        nonstring_task1 = SimpleTask(some_list)
-        
-
-    except ValueError as e:
-        print(f"A ValueError was properly caught: {e}")
-    
-    print("")
-    
-    # EMPTY TASK VIOLATION
-    print("")
-    try:
-        print("trying...")
-        
-        empty_task = SimpleTask("")
-        empty_task.show_details()
-
-    except ValueError as e:
-        print(f"A ValueError was properly caught: {e}")
-    
-    print("")
-    
-    
-    # WHITESPACE TASK VIOLATION
-    print("")
-    try:
-        print("trying...")
-        whitespace_task = ComplexTask(" ")
-        whitespace_task.show_details()
-        
-    except ValueError as e:
-        print(f"A ValueError was properly caught: {e}")
-    
-    print("")
-    
-    
-    try:
-        nonalphanumeric_task1 = ComplexTask("Apostrophe's Breakage")
-        nonalphanumeric_task1.show_details()
-        nonalphanumeric_task2 = ComplexTask("No Punctuation!! Cuz They Might Become Slip Titles!!!")
-        nonalphanumeric_task2.show_details()
-        print("if you can read this, they aren't failing properly")
-    except ValueError as e:
-        print(f"A ValueError occured: {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {type(e).__name__} – {e}")
-
---
+++ tests.py
@@ -0,0 +1,83 @@
+from ariadne import SimpleTask, ComplexTask
+
+print("these next steps should all throw an error and get caught properly, if it does not, its not working yet, we'll move them to an error catching block when its confirmed that they throw errors (this is a quick and dirty Red Green Test from TDD principles)")
+print("")
+print("If this script ends with 'An unexpected error occurred' it means they weren't caught properly")
+
+# NONSTRING TASK VIOLATION
+task_violation_name = "NONSTRING"
+print("")
+try:
+    print(f"trying {task_violation_name} task violation...")
+    some_list = ["string", "list", "not supported"]
+    nonstring_task1 = SimpleTask(some_list)
+    
+
+except ValueError as e:
+    print(f"A ValueError was properly caught: {e}")
+
+print("")
+
+# EMPTY TASK VIOLATION
+task_violation_name = "EMPTY"
+print("")
+try:
+    print(f"trying {task_violation_name} task violation...")
+    
+    empty_task = SimpleTask("")
+    empty_task.show_details()
+
+except ValueError as e:
+    print(f"A ValueError was properly caught: {e}")
+
+print("")
+
+
+# WHITESPACE TASK VIOLATION
+task_violation_name = "WHITESPACE"
+print("")
+try:
+    print(f"trying {task_violation_name} task violation...")
+    whitespace_task = ComplexTask(" ")
+    whitespace_task.show_details()
+    
+except ValueError as e:
+    print(f"A ValueError was properly caught: {e}")
+
+print("")
+
+# APOSTROPHE TASK VIOLATION
+task_violation_name = "APOSTROPHE"
+print("")
+try:
+    print(f"trying {task_violation_name} task violation...")
+    nonalphanumeric_task1 = ComplexTask("Apostrophe's Breakage")
+    nonalphanumeric_task1.show_details()
+    
+except ValueError as e:
+    print(f"A ValueError was properly caught: {e}")
+
+print("")
+
+# PUNCTUATION TASK VIOLATION
+task_violation_name = "PUNCTUATION"
+print("")
+try:
+    print(f"trying {task_violation_name} task violation...")
+    nonalphanumeric_task2 = ComplexTask("No Punctuation!! Cuz They Might Become Slip Titles!!!")
+    nonalphanumeric_task2.show_details()
+    
+except ValueError as e:
+    print(f"A ValueError was properly caught: {e}")
+
+print("")
+
+try:
+    nonalphanumeric_task2 = ComplexTask("No Punctuation!! Cuz They Might Become Slip Titles!!!")
+    nonalphanumeric_task2.show_details()
+    print("if you can read this, they aren't failing properly")
+except ValueError as e:
+    print(f"A ValueError occured: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {type(e).__name__} – {e}")
+
